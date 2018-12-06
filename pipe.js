322aa81c6bd36003cb3371fa93c7d6190bb7dfab
Andrea Simone Costa <jfet97@users.noreply.github.com>
2018-12-06 19:23:51 +0100

    Create pipe.js

---
+++ pipe.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const pipe = function () {
+    const functionalPipe = (fns) => x => fns.reduce((v, f) => f(v), x);
+    
+    return async function pipe(inputStream, outputStream, ...modifiers) {
+
+        const pipedFunctions = modifiers.length ? functionalPipe(modifiers) : null;
+
+        for await (const chunk of inputStream) {
+            if (pipedFunctions) outputStream.write(pipedFunctions(chunk));
+            else outputStream.write(chunk);
+        }
+        
+        return outputStream;
+    }
+}();
+
+exports.pipe = pipe;
